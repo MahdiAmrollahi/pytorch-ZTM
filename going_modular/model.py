7d44f6536735d70cd5e3710a3778da2a21e1f1f9
dark-noob830 <mahdiamr83@gmail.com>
2026-04-11 13:48:04 +0330

    Script mode for make model

---
+++ going_modular/model.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape: int,
+                        hidden_units: int,
+                        output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                        out_channels=hidden_units,
+                        kernel_size=3,
+                        stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                        out_channels=hidden_units,
+                        kernel_size=3,
+                        stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                        out_channels=hidden_units,
+                        kernel_size=3,
+                        stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                        out_channels=hidden_units,
+                        kernel_size=3,
+                        stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units * 13 * 13,
+                        out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor):
+        # x = self.conv_block_1(x)
+        # print(x.shape)
+        # x = self.conv_block_2(x)
+        # print(x.shape)
+        # x = self.classifier_layer(x)
+        # print(x.shape)
+        # return x
+
+        return self.classifier_layer(
+            self.conv_block_2(self.conv_block_1(x))
+            )
